a12ee7e1be8fa52352b8dd9fb4448c9959d746c2
Arno Haase <arno.haase@haase-consulting.com>
2018-01-03 00:18:17 +0100

    cleanup

--- src/main/scala/com/ajjpj/simpleakkadowning/Helpers.scala
+++ src/main/scala/com/ajjpj/simpleakkadowning/Helpers.scala
@@ -1,82 +1,16 @@
 package com.ajjpj.simpleakkadowning
 
-import java.time.format.DateTimeFormatter
-import java.time.{Instant, LocalDateTime, ZoneId}
-import java.util.{Comparator, Locale}
+import java.util.Locale
 import java.util.concurrent.TimeUnit
-import java.util.regex.Pattern
 
 import com.typesafe.config.Config
 
-import scala.annotation.tailrec
 import scala.concurrent.duration.{Duration, FiniteDuration}
 
 
 private[simpleakkadowning] object Helpers {
   def toRootLowerCase(s: String) = s.toLowerCase(Locale.ROOT)
 
-  val isWindows: Boolean = toRootLowerCase(System.getProperty("os.name", "")).indexOf("win") >= 0
-
-  def makePattern(s: String): Pattern = Pattern.compile("^\\Q" + s.replace("?", "\\E.\\Q").replace("*", "\\E.*\\Q") + "\\E$")
-
-  def compareIdentityHash(a: AnyRef, b: AnyRef): Int = {
-    /*
-     * make sure that there is no overflow or underflow in comparisons, so
-     * that the ordering is actually consistent and you cannot have a
-     * sequence which cyclically is monotone without end.
-     */
-    val diff = ((System.identityHashCode(a) & 0xffffffffL) - (System.identityHashCode(b) & 0xffffffffL))
-    if (diff > 0) 1 else if (diff < 0) -1 else 0
-  }
-
-  /**
-    * Create a comparator which will efficiently use `System.identityHashCode`,
-    * unless that happens to be the same for two non-equals objects, in which
-    * case the supplied “real” comparator is used; the comparator must be
-    * consistent with equals, otherwise it would not be an enhancement over
-    * the identityHashCode.
-    */
-  def identityHashComparator[T <: AnyRef](comp: Comparator[T]): Comparator[T] = new Comparator[T] {
-    def compare(a: T, b: T): Int = compareIdentityHash(a, b) match {
-      case 0 if a != b ⇒ comp.compare(a, b)
-      case x           ⇒ x
-    }
-  }
-
-  /**
-    * Converts a "currentTimeMillis"-obtained timestamp accordingly:
-    * {{{
-    *   "$hours%02d:$minutes%02d:$seconds%02d.$ms%03dUTC"
-    * }}}
-    *
-    * @param timestamp a "currentTimeMillis"-obtained timestamp
-    * @return the formatted timestamp
-    */
-  def currentTimeMillisToUTCString(timestamp: Long): String = {
-    val timeOfDay = timestamp % 86400000L
-    val hours = timeOfDay / 3600000L
-    val minutes = timeOfDay / 60000L % 60
-    val seconds = timeOfDay / 1000L % 60
-    val ms = timeOfDay % 1000
-    f"$hours%02d:$minutes%02d:$seconds%02d.$ms%03dUTC"
-  }
-
-  private val formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss.SSS")
-  private val timeZone = ZoneId.systemDefault()
-
-  def timestamp(time: Long): String = {
-    formatter.format(LocalDateTime.ofInstant(Instant.ofEpochMilli(time), timeZone))
-  }
-
-  final val base64chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789+~"
-
-  @tailrec
-  def base64(l: Long, sb: java.lang.StringBuilder = new java.lang.StringBuilder("$")): String = {
-    sb append base64chars.charAt(l.toInt & 63)
-    val next = l >>> 6
-    if (next == 0) sb.toString
-    else base64(next, sb)
-  }
 
   /**
     * Implicit class providing `requiring` methods. This class is based on
